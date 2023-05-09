d638a358710d4ce7efd33264c6ed3a79cae47a57
torreschien <torreschien@gmail.com>
2023-05-09 21:43:25 +0800

    ChangeFolder

--- PeterPan/0505/test.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import AVFAudio
-
-let utterance = AVSpeechUtterance(string: "오늘이 한 칸이 채 안 남은 그런 시간 우리 소원을 빌며 웃던 그 시간 별 게 다 널 떠오르게 하지")
-
-utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
-utterance.rate = 0.5
-
-let synthesizer = AVSpeechSynthesizer()
-synthesizer.speak(utterance)
